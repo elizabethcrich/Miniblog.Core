a79dcbc68b69ddd5a7d23c4324430408f350bea1
ecrichlo <elizabeth.crichlow@gmail.com>
2019-12-13 12:47:30 -0500

    attempt to create HomeVM to get/check Categories for nav links
    just don't need it yet, come back to this later

--- src/Controllers/HomeController.cs
+++ src/Controllers/HomeController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Miniblog.Core.Services;
+using Miniblog.Core.ViewModels;
+using WebEssentials.AspNetCore.Pwa;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,6 +14,16 @@ namespace Miniblog.Core.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IBlogService _blog;
+        private readonly IOptionsSnapshot<BlogSettings> _settings;
+        private readonly WebManifest _manifest;
+
+        public HomeController(IBlogService blog, IOptionsSnapshot<BlogSettings> settings, WebManifest manifest)
+        {
+            _blog = blog;
+            _settings = settings;
+            _manifest = manifest;
+        }
         // GET: /<controller>/
         public IActionResult Index()
         {
@@ -19,6 +33,19 @@ namespace Miniblog.Core.Controllers
         public IActionResult About()
         {
             return View("About");
+        }
+
+        public async Task<IEnumerable<string>> GetCategories()
+        {
+            return await _blog.GetCategories();
+        }
+
+        private async Task<HomeVM> GetHomeVM()
+        {
+            return new HomeVM
+            {
+                Categories = await GetCategories()
+            };
         }
     }
 }
---
+++ src/ViewModels/HomeVM.cs
@@ -0,0 +1,13 @@
+﻿using Miniblog.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Miniblog.Core.ViewModels
+{
+    public class HomeVM
+    {
+        public IEnumerable<string> Categories { get; set; }
+    }
+}
